1015f32bdbf00f83ef3686a6505c6fd9f8be93a9
Cofiee <marvorginal@gmail.com>
2022-04-09 14:33:32 +0200

    React and Three.js
    First commit. I set up React and created Three js canva.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ThreeCanva from './components/ThreeCanva/ThreeCanva';
+
+function App() {
+  return (
+    <div className="App">
+      <ThreeCanva name="ThreeCanva" />
+    </div>
+  );
+}
+
+export default App;
